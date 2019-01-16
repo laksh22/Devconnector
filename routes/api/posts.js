9d8d657b1a7a19b4cda4a9a763606c66b3e17755
Laksh <lakshyajeet22@gmail.com>
2019-01-17 00:44:03 +0800

    Add authentication with passport

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // @route GET api/posts/test
 // @desc Tests posts route
-// @access Public route
+// @access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
 module.exports = router;
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // @route GET api/profiles/test
 // @desc Tests profiles route
-// @access Public route
+// @access Public
 router.get("/test", (req, res) => res.json({ msg: "Profiles Works" }));
 
 module.exports = router;
